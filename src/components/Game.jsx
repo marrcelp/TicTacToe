39ca2097529d48eb93bad54f6a018a3119b24b38
marrcelp <marcel.piaszczyk@gmail.com>
2024-04-07 13:11:54 +0200

    added restart button and player info, added winning patterns

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -27,12 +27,28 @@ const Game = () => {
         setIsXturn(!isXturn);
         setSquares(squaresCopy);
 
-
-
     }
+
+    function handleRestart(){
+        setSquares(Array(9).fill(''));
+    }
+
+    const winningPatterns = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+
+
 
     return (
         <div className='game_container'>
+
                 <div className='row'>
                     <Square value={squares[0]} onClickHandler={() => handleClick(0)}/>
                     <Square value={squares[1]} onClickHandler={() => handleClick(1)}/>
@@ -50,6 +66,15 @@ const Game = () => {
                     <Square value={squares[7]} onClickHandler={() => handleClick(7)}/>
                     <Square value={squares[8]} onClickHandler={() => handleClick(8)}/>
                 </div>
+
+                <div className='row'>
+                    <p>{isXturn ? `It's X turn` : `It's O turn` }</p>
+                </div>
+
+                <div className='row'>
+                    <button onClick={handleRestart}>RESTART THE GAME</button>
+                </div>
+
         </div>
     );
 };
